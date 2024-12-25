da7ea4cc51b58a53172a56854fbe94fd3db799a7
Caizi Jiangnan <h4627488@outlook.com>
2024-12-26 01:23:22 +0800

    更新count_lines.py以排除特定目录的行数统计，修改Makefile以包含end.md作为总结部分，并在end.md中添加项目总结和代码行数统计

--- count_lines.py
+++ count_lines.py
@@ -1,6 +1,7 @@
 import os
 
 exts = ['.cpp', '.c', '.h']
+avoid_dirs = ['.git','docs']
 
 def count_lines_in_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -9,13 +10,16 @@ def count_lines_in_file(file_path):
 def count_lines_in_directory(directory):
     total_lines = 0
     for root, _, files in os.walk(directory):
-        # print(root)
+        if any(avoid_dir in root for avoid_dir in avoid_dirs):
+            continue
+        root_lines = 0
         for file in files:
             if os.path.splitext(file)[1] in exts:
                 file_path = os.path.join(root, file)
                 file_lines = count_lines_in_file(file_path)
-                print(f"{file_path}: {file_lines}")
-                total_lines += file_lines
+                root_lines += file_lines
+        print(f"{root}: {root_lines}")
+        total_lines += root_lines
     return total_lines
 
 if __name__ == "__main__":
